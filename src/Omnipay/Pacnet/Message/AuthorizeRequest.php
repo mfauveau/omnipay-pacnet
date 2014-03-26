21081f07ecee78710842db32f5a102f3a4defe25
Matthieu Fauveau <mfauveau@gmail.com>
2014-03-26 11:46:09 -0400

    Allow a call to purchase with only a transactionReference for recurring payments

--- src/Omnipay/Pacnet/Message/AuthorizeRequest.php
+++ src/Omnipay/Pacnet/Message/AuthorizeRequest.php
@@ -11,19 +11,25 @@ class AuthorizeRequest extends SubmitRequest
     {
         $data = parent::getData();
 
-        $this->validate('card');
-
         $data['PymtType'] = 'cc_preauth';
-        $data['CardBrand'] = $this->getCard()->getBrand();
-        $data['CardNumber'] = $this->getCard()->getNumber();
-        $data['ExpiryDate'] = $this->getCard()->getExpiryDate('my');
 
-        if ($this->getCard()->getCvv()) {
-            $data['CVV2'] = $this->getCard()->getCvv();
+        if ( ! $this->getTransactionReference()) {
+            $this->validate('card');
+
+            $data['CardBrand'] = $this->getCard()->getBrand();
+            $data['CardNumber'] = $this->getCard()->getNumber();
+            $data['ExpiryDate'] = $this->getCard()->getExpiryDate('my');
+
+            if ($this->getCard()->getCvv()) {
+                $data['CVV2'] = $this->getCard()->getCvv();
+            }
+
+            if ($this->getCard()->getName()) {
+                $data['AccountName'] = $this->getCard()->getName();
+            }
         }
-
-        if ($this->getCard()->getName()) {
-            $data['AccountName'] = $this->getCard()->getName();
+        else {
+            $data['TemplateNumber'] = $this->getTransactionReference();
         }
 
         $data['Signature'] = $this->generateSignature($data);
